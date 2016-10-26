e3699413ffb6966dd13a81a9f9836d1482b4e736
Trielkuiri <sasha.t5@mail.ru>
2016-10-26 19:53:06 +0400

    new password

---
+++ test.py
@@ -0,0 +1,30 @@
+c=4
+cc=1
+password=('fool')
+
+while cc<c:
+      if password!=('oche_bolshaya_jopa'):
+            print('попытка номер', cc, 'из 3')
+            password=input('password:  ')
+            cc=cc+1
+      else:
+            print('одобрено')
+            cc=cc+10
+if cc==4:
+      print('попытки кончились')
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
